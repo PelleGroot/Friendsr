1d2e538a8eccb9ab602bb8f8201ad5125d9ea3cb
PelleGroot <pellegroot@gmail.com>
2018-02-19 19:46:05 +0100

    added the adapter

---
+++ app/src/main/java/nl/pellegroot/friendsr/FriendsAdapter.java
@@ -0,0 +1,28 @@
+package nl.pellegroot.friendsr;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+    public FriendsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Friend> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+        return convertView;
+    }
+}
